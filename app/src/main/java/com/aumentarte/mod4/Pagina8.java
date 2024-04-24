f778aefb33864fce4a3315a3bbc3477a20c5bc9a
Daniela-Garrido <vj.nefertiti@gmail.com>
2024-04-23 23:25:48 -0400

    Initial commit

---
+++ app/src/main/java/com/aumentarte/mod4/Pagina8.java
@@ -0,0 +1,14 @@
+package com.aumentarte.mod4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pagina8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagina8);
+    }
+}
